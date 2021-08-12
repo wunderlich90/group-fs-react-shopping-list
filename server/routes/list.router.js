741243fe709e192fe6781354d8dec730dadd70c1
Jeremy Cady <jcady90@icloud.com>
2021-08-12 11:19:00 -0500

    router.get created

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,22 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+router.get('/', (res, req) => {
+    const sqlQuery = `SELECT * FROM "shopping_list",
+                        ORDER BY "isPurchased" ASC,
+                        "name" ASC;`;
+    pool.query(sqlQuery)
+        .then((result) => {
+            console.log('Received data from DB', result);
+            res.send(result.rows);   
+        })
+        .catch((error) => {
+            console.log('Database query error', error);
+            res.sendStatus(500);
+            
+        })
+    
+})
+
 
 module.exports = router;
