741807a36abb7f8376aeef48950b9ba42f2c626b
Jeremy Cady <jcady90@icloud.com>
2021-08-12 10:33:32 -0500

    DB created

--- database.sql
+++ database.sql
@@ -1,2 +1,18 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "shopping_list"(
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR(80),
+"quantity" FLOAT,
+"unit" VARCHAR(20),
+"isPurchased" BOOLEAN DEFAULT false
+);
+
+INSERT INTO "shopping_list"(
+"name",
+"quantity" ,
+"unit",
+"isPurchased")
+
+VALUES ('milk', 1, 'gallon', false);
